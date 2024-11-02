c17d26c3e10cd6754156d5753980b93852c8546a
VitorMarciano001 <vitor.marciano001@gmail.com>
2024-11-02 20:16:51 -0300

    feat: criado tela homepage

---
+++ src/paginas/homepage/home_page.php
@@ -0,0 +1,59 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="icon" type="image/x-icon" href="../../../img/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    <title>Marmitaria Fit</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light nav_style">
+        <div class="container-fluid align-items-center" style="height: 60px;">
+            <div >
+                <img src="../../../img/logo.png" alt="Marmitaria Fit Logo" width="60">
+                <a class="navbar-brand ms-2">Marmitaria Fit</a>
+            </div>
+            <div>
+                <a href="../../../controle/logout.php" class="btn btn-danger">Sair</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="menu_lateral">
+        <ol class="menu_list">
+            <li>
+                <a >
+                    <img class="menu" src="../../../icons/menu.svg" alt="" width="50px">
+                </a>
+            </li>
+            <li>
+                <a href="../../comanda/comanda.php" >
+                    <img src="../../../icons/comanda.svg" alt="" width="50px">
+                </a>
+            </li>
+            <li>
+                <a href="../../estoque/estoque.php">
+                    <img src="../../../icons/estoque.svg" alt="" width="50px">
+                </a>
+            </li>
+        </ol>
+    </div>
+
+    <div class="content">
+        <p style="text-align: center;">Seja Bem Vindo,<br>Vitor</p>
+    </div>
+</body>
+
+</html>
