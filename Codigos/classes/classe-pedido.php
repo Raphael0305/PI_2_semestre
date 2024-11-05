ca240f96b76d2df942a1c063290d4d6ffbecacca
Raphael <162832824+Raphael0305@users.noreply.github.com>
2024-11-05 00:32:27 -0300

    atualização código
    atualização

--- Codigos/classes/classe-pedido.php
+++ Codigos/classes/classe-pedido.php
@@ -1,36 +1,34 @@
 <?php
- require_once '/xampp/htdocs/MarmitariaProj/classes/classe-conexao.php';
+ require_once 'classe-conexao.php';
  
 class Pedido {
     private $conectar;
  
-    public function __construct(ConexaoBanco $conexao)  {
+    public function __construct()  {
+        $conexao = NEW ConexaoBanco;
         $this->conectar = $conexao->getConexao();
     }
 //************************************************************************************************************************************************************* */ 
-    public function buscarDados($pagina, $limite) {
-        $inicio = ($pagina * $limite) - $limite;
+public function buscarDados($pagina, $limite) {
+    $inicio = ($pagina * $limite) - $limite;
 
-        $res = array();    
-        $cmd = $this->conectar->prepare("
-            SELECT id_pedido, Nome_Marmita, Nome_Cliente, quantidade, 
-                   DATE_FORMAT(dataEntrega, '%d/%m/%Y %H:%i') AS dataEntregaFormatada 
-            FROM pedidos 
-            ORDER BY dataEntrega ASC
-            LIMIT :inicio, :limite
-        ");
-        
-        $cmd->bindParam(':inicio', $inicio, PDO::PARAM_INT);
-        $cmd->bindParam(':limite', $limite, PDO::PARAM_INT);
-        
-        $cmd->execute();
-        
-        $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
-        return $res;
-    }
+    $res = array();
+    $cmd = $this->conectar->prepare("
+        SELECT id_pedido, nome_marmita, nome_cliente, quantidade, 
+               DATE_FORMAT(data_entrega, '%d/%m/%Y %H:%i') AS dataEntregaFormatada 
+        FROM pedidos 
+        ORDER BY data_entrega ASC
+        LIMIT $inicio, $limite
+    ");
+    
+    $cmd->execute();
+    
+    $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
+    return $res;
+}
 //************************************************************************************************************************************************************* */ 
     public function cadastrarPedido($marmita, $nome,$qtde,$data){
-        $cmd = $this->conectar->prepare("INSERT INTO pedidos (Nome_Marmita, Nome_Cliente, quantidade, dataEntrega) VALUES (:m, :n, :q, :d)");
+        $cmd = $this->conectar->prepare("INSERT INTO pedidos (nome_marmita, nome_cliente, quantidade, data_entrega) VALUES (:m, :n, :q, :d)");
         $cmd->bindValue(":m", $marmita);
         $cmd->bindValue(":n", $nome);
         $cmd->bindValue(":q", $qtde);
@@ -49,9 +47,9 @@ class Pedido {
     }
 //************************************************************************************************************************************************************* */ 
     public function EditarPedido($id, $nomeMarmita, $nomeCliente,$qtde,$data){
-        $cmd = $this->conectar->prepare("UPDATE pedidos SET Nome_Marmita = :m, 
-                                        Nome_Cliente = :c,
-                                        quantidade = :q, dataEntrega = :d
+        $cmd = $this->conectar->prepare("UPDATE pedidos SET nome_marmita = :m, 
+                                        nome_cliente = :c,
+                                        quantidade = :q, data_entrega = :d
                                         WHERE id_pedido = :id");
         $cmd->bindValue(":m", $nomeMarmita);
         $cmd->bindValue(":c", $nomeCliente);
