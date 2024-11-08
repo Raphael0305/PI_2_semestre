f4c62b939c6cd1e2217c1ccd0cb82c6bc59ae327
VitorMarciano001 <vitor.marciano001@gmail.com>
2024-11-07 21:05:10 -0300

    tela de estoque

--- src/paginas/estoque/estoque.php
+++ src/paginas/estoque/estoque.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/content.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="assets/img/logo.png">
+    <script src="https://kit.fontawesome.com/e874ed8d35.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Estoque Fit</title>
 </head>
@@ -59,12 +60,16 @@
 
                 <div class="table_side">
                     <div class="search_box">
-                        <input type="text" placeholder="Buscar Produto">
+                        <input class="serach_input" type="text" placeholder="Buscar Produto">
+                        <div>
+                            <button class="search_btn">
+                                <i class="fa-solid fa-magnifying-glass fa-beat" style="--fa-animation-duration: 2s; color:white;"></i>
+                            </button>
+                        </div>
                     </div>
                     <div class="table_box">
                         <table class="tables">
                             <thead>
-
                                 <tr>
                                     <th>Nome</th>
                                     <th>Quantidade</th>
@@ -80,20 +85,35 @@
                                     <td>KG</td>
                                 </tr>
                                 <tr>
-                                    <td>AFeijao Pretinho</td>
+                                    <td>Feijao Pretinho</td>
                                     <td>4.560</td>
                                     <td>R$23,90</td>
                                     <td>KG</td>
+                                </tr>
+                                <tr>
+                                    <td>Feijao Pretinho</td>
+                                    <td>4.560</td>
+                                    <td>R$23,90</td>
+                                    <td>KG</td>
+                                </tr>
+                                <tr>
+                                    <td>Feijao Pretinho</td>
+                                    <td>4.560</td>
+                                    <td>R$23,90</td>
+                                    <td>KG</td>
+                                </tr>
                                 <tr>
                                     <td>Macarrao macarronado</td>
                                     <td>0.900</td>
                                     <td>R$4.00</td>
                                     <td>KG</td>
+                                </tr>
                                 <tr>
                                     <td>Leite de pedra</td>
                                     <td>6</td>
                                     <td>R$7,90</td>
                                     <td>LT</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -108,11 +128,11 @@
 
 
                 <div class="btn_side">
-                    <button class="cadastrar" onclick="cadastrarProduto">Cadastrar ingrediente</button>
-                    <button class="cadastrar" onclick="cadastrarProduto">Adicionar item</button>
-                    <button class="cadastrar" onclick="cadastrarProduto">Retirar item do estoque</button>
-                    <button class="cadastrar" onclick="cadastrarProduto">Retirada de Estoque</button>
-                    <button class="cadastrar" onclick="cadastrarProduto">Criar alerta de baixo nível</button>
+                    <button class="cadastrar">Cadastrar ingrediente</button>
+                    <button class="cadastrar">Adicionar item</button>
+                    <button class="cadastrar">Retirar item do estoque</button>
+                    <button class="cadastrar">Retirada de Estoque</button>
+                    <button class="cadastrar">Criar alerta de baixo nível</button>
 
                 </div>
             </div>
