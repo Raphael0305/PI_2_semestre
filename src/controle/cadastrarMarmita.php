1c966f22f90cf6b6745d72b173d3e47bdfa46290
Raphael <162832824+Raphael0305@users.noreply.github.com>
2024-12-01 17:04:34 -0300

    merge AjustePorcedure, cadastro marmita ocm precudre

--- src/controle/cadastrarMarmita.php
+++ src/controle/cadastrarMarmita.php
@@ -1,9 +1,8 @@
 <?php
-include_once  __DIR__ . '/../modelo/query.php';
+include_once __DIR__ . '/../modelo/query.php';
 header('Content-Type: application/json');
 
 $dados = json_decode(file_get_contents("php://input"), true);
-
 
 if (!$dados) {
     echo json_encode(['status' => 'erro', 'mensagem' => 'Dados inválidos']);
@@ -14,19 +13,13 @@ $nomeMarmita = $dados['nomeMarmita'];
 $precoMarmita = (float)$dados['precoMarmita']; 
 $ingredientes = $dados['ingredientes'];
 
-
 try {
     $query = new Query();
 
-    $idMarmita = $query->cadastrarMarmita($nomeMarmita, $precoMarmita);
-
-
-    $query->cadastrarIngredientes($idMarmita, $ingredientes);
-
+    $query->cadastrarMarmitaComProcedure($nomeMarmita, $precoMarmita, $ingredientes);
 
     echo json_encode([
         'status' => 'sucesso',
-        'ID_marmita' => $idMarmita,
         'nomeMarmita' => $nomeMarmita,
         'precoMarmita' => $precoMarmita,
         'ingredientes' => $ingredientes
@@ -34,4 +27,3 @@ try {
 } catch (Exception $e) {
     echo json_encode(['status' => 'erro', 'mensagem' => $e->getMessage()]);
 }
-?>
